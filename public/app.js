082f0c633f479d412580329e50507a0553b88300
KOMATSU Seiji <comutt.jp@gmail.com>
2017-07-29 23:25:49 +0900

    Added routing

--- public/app.js
+++ public/app.js
@@ -1,6 +1,18 @@
 'use strict';
+
 var learnjs = {};
-learnjs.showView = function(hash) {
+
+learnjs.problemView = function() {
   var problemView = $('<div class="problem-view">').text('Coming soon!');
   $('.view-container').empty().append(problemView);
+}
+
+learnjs.showView = function(hash) {
+  var routes = {
+    '#problem-1': learnjs.problemView
+  };
+  var viewFn = routes[hash];
+  if (viewFn) {
+    $('.view-container').empty().append(viewFn());
+  }
 }
